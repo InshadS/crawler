e2ac19fefc1489949d8b2bc38f553898101c6294
InshadS <inshad1504@gmail.com>
2022-03-17 17:33:11 +0530

    changed the code to print the songs and lyrics directly without a directory

--- crawler.py
+++ crawler.py
@@ -33,26 +33,14 @@ def get_lyrics(song_url):
     return lyrics
 
 def crawl():
-    base_dir = "lyrics"
-    try:
-        os.mkdir(base_dir)
-    except Exception:
-        pass
     artists= get_artists("https://www.songlyrics.com/a/")
     for name, link in artists:
         print(name, "   :   ",link)
-        name_dir = os.path.join(base_dir, name.replace(" ", "_").lower())
-        try:
-            os.mkdir(name_dir)
-        except Exception:
-            pass
         songs = get_songs(link)
         for song, song_link in songs:
+            print("\n-----------------------------\n",song,"\n-----------------------------\n")
             lyrics = get_lyrics(song_link)
-            song_file = os.path.join(name_dir, song.replace(" ", "_").lower()+".txt")
-            with open(song_file, "w") as f:
-                f.write(lyrics)
-            print (".", end="", flush=True)
+            print (lyrics,"\n\n*****************************")
         print("DONE")
     
 if __name__=="__main__":
